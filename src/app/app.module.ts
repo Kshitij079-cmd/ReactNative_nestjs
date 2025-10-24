2378f478866beead156b8575ee9fd5921c3f0de3
Kshitij079-cmd <rajvanshikshitij@gmail.com>
2025-10-24 13:11:35 +0530

    added send-otp post endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from 'src/users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,10 +3,20 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+
+@Controller('send-otp')
+export class AuthController {
+  @Post()
+  sentOTP(@Body('message') message: string) {
+    return { response: `You sent: ${message}` };
+  }
+}
+
+
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
